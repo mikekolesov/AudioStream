31fb35cb370657b2a44efd07a85f436b7a230fee
Michael Kolesov <michael@MikeMacbook.local>
2012-11-06 18:05:09 +0400

    Designed app icon with monogram 'AS' added for general and Retina display of iPhone and iPad. Launch images for iPhone and iPhone(Retina) added.

--- AudioStream/ASMasterViewController.m
+++ AudioStream/ASMasterViewController.m
@@ -24,6 +24,9 @@
     if (self) {
         self.title = @"Audio Stream";
         
+        // just for generating launch image
+        //self.title = @"Loading...";
+        
         // change back title
         UIBarButtonItem *bb = [[[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:nil action:nil] autorelease];
         self.navigationItem.backBarButtonItem = bb;
@@ -40,7 +43,8 @@
     }
     return self;
 }
-							
+
+
 - (void)dealloc
 {
     [_detailViewController release];
@@ -61,8 +65,10 @@
 //    UIBarButtonItem *addButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)] autorelease];
 //    self.navigationItem.rightBarButtonItem = addButton;
     
+    // comment code below to generate launch image
     UIBarButtonItem *newButton = [[[UIBarButtonItem alloc] initWithTitle:@"New" style:UIBarButtonItemStyleBordered target:self action:@selector(insertNewObject:)] autorelease];
     self.navigationItem.rightBarButtonItem = newButton;
+    // last line of launch image comment
 }
 
 - (void)viewDidUnload
