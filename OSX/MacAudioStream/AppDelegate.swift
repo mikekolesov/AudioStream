c5a0a1edd8e8025a6290ef6700a8cb7a0ade0aba
Michael Kolesov <mikhail.kolesov@firstlinesoftware.com>
2015-12-25 15:40:59 +0300

    Changed the path of common lib. Mac app replaced with Swift

---
+++ OSX/MacAudioStream/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAudioStream
+//
+//  Created by MKolesov on 25/12/15.
+//  Copyright © 2015 Michael Kolesov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSX/MacAudioStream/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAudioStream
+//
+//  Created by MKolesov on 25/12/15.
+//  Copyright © 2015 Michael Kolesov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
